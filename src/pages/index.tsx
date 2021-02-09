5a5b5bdb86f60d12113b0a0fcbc26ad1729cf95d
PabloMelo <pablomelodev@gmail.com>
2021-02-09 17:49:13 -0300

    feat: used server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react';
-import { Title } from '../styles/pages/Home';
+import { GetServerSideProps } from 'next';
 
+import { Title } from '../styles/pages/Home';
 interface IProduct {
   id: string;
   title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>(
-    []
-  );
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
 
-  useEffect(() => {
-    fetch('http://localhost:3333/recommended').then((response) => {
-      response.json().then((data) => {
-        setRecommendedProducts(data);
-      });
-    });
-  }, []);
-
+export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
       <section>
@@ -35,3 +27,14 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts,
+    },
+  };
+};
